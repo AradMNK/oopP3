80dd8abc2489e377145560bb56373d582b58b35a
AradMNK <aradkashani@yahoo.com>
2022-08-03 16:24:59 +0430

    feedbuilder preps

--- src/main/java/graphics/app/UserFXML.java
+++ src/main/java/graphics/app/UserFXML.java
@@ -6,63 +6,47 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
-import Objects.Post;
 import Objects.User;
 import javafx.scene.text.Text;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 
 public class UserFXML {
     @FXML Button followButton;
     @FXML Circle picture;
-    @FXML Text name, username, bio, postContent, subtitle, date;
-    @FXML ScrollPane postPane, bioPane;
+    @FXML Text name, username, bio, subtitle, date;
+    @FXML ScrollPane bioPane;
     @FXML GridPane picturePane;
 
-    private void initContents(String name, String username, String bio, String post, String subtitle, LocalDateTime time){
+    private void initContents(String name, String username, String bio, String subtitle, LocalDate time){
         this.name.setText(name);
         this.username.setText("@" + username);
         this.bio.setText(bio);
-        this.postContent.setText(post);
         this.subtitle.setText(subtitle);
         this.date.setText(time.format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss")));
-
-        postContent.wrappingWidthProperty().bind(postPane.widthProperty().subtract(Utility.POST_TEXT_MARGIN_FROM_RIGHT));
         this.bio.wrappingWidthProperty().bind(bioPane.widthProperty().subtract(Utility.POST_TEXT_MARGIN_FROM_RIGHT));
         picture.radiusProperty().bind(picturePane.heightProperty().divide(2));
     }
 
-    private void initPicture(String handle) {
-        if (handle.equals("")) return;
 
-        ImageView imageView = new ImageView();
-        imageView.setImage(new Image(handle));
-    }
-
-    public void initialize(Post post){
-        if (post.getPoster().getPfp().getHandle().equals(""))
+    public void initialize(User user){
+        if (user.getPfp().getHandle().equals(""))
             picture.setFill(new ImagePattern(new Image
                     ((Objects.requireNonNull(Launcher.class.getResource(Utility.UNKNOWN_USER_PICTURE))).toString())));
         else{
-            try {picture.setFill(new ImagePattern(new Image(post.getPoster().getPfp().getHandle())));}
+            try {picture.setFill(new ImagePattern(new Image(user.getPfp().getHandle())));}
             catch (IllegalArgumentException e){AppManager.alert(Alert.AlertType.ERROR, "Unsupported image file!",
                     "Please choose another image.", "Image could not load!");
                 picture.setFill(new ImagePattern(new Image((Objects.requireNonNull
                         (Launcher.class.getResource(Utility.UNKNOWN_USER_PICTURE))).toString())));}
         }
 
-        User poster = post.getPoster();
-        initContents(poster.getName(), poster.getUsername(), poster.getBio(),
-                post.getDescription(), poster.getSubtitle(), post.getDatePosted());
-        initPicture(post.getPicture().getHandle());
-
-        //FIXME button
+        initContents(user.getName(), user.getUsername(), user.getBio(), user.getSubtitle(), user.getDateJoined());
     }
 
     @FXML void follow(){
