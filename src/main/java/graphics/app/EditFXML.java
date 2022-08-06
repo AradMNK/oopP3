af2aa2c5853c2361d9d7cc64ec9e8b1a4ec5484b
AradMNK <aradkashani@yahoo.com>
2022-08-06 10:57:31 +0430

    fixed messages

--- src/main/java/graphics/app/EditFXML.java
+++ src/main/java/graphics/app/EditFXML.java
@@ -2,14 +2,16 @@ package graphics.app;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 
 public class EditFXML {
+    @FXML TextArea msg;
     @FXML Button cancelButton, editButton;
 
     @FXML void cancel(){
-
+        ChatFXML.chatFXML.cancelEdit();
     }
     @FXML void confirm(){
-
+        ChatFXML.chatFXML.confirmEdit();
     }
 }
