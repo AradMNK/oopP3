c18fad41bd68b8a8cb7561eecfeb04913dba9e05
AradMNK <aradkashani@yahoo.com>
2022-08-07 16:45:40 +0430

    debugged messages!

--- src/main/java/graphics/app/SearchFXML.java
+++ src/main/java/graphics/app/SearchFXML.java
@@ -1,14 +1,16 @@
 package graphics.app;
 
-import Database.Changer;
+import Builder.GroupMessageBuilder;
+import Builder.MessageBuilder;
+import Builder.UserBuilder;
+import Database.Loader;
 import Login.Loginner;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Alert;
 import javafx.scene.layout.VBox;
-import Objects.Post;
-import Objects.Comment;
-import Objects.Like;
+import Objects.GroupMessage;
+import Objects.Message;
 
 import java.io.IOException;
 import java.util.*;
@@ -17,12 +19,9 @@ public class SearchFXML {
     @FXML
     VBox displayUsers, displayDirects, displayGroups;
 
-    public void initialize(HashSet<Post> posts, HashSet<Comment> comments, HashSet<Like> likes){
-        List<Post> postList = posts.stream().toList();
-        ArrayList<Post> sortedPost = new ArrayList<>(postList);
-        sortedPost.sort(Comparator.comparing(Post::getDatePosted));
-
-        if (sortedPost.size() == 0){
+    public void initialize(String pattern){
+        String[] usernames = Loader.searchForUsers(pattern);
+        if (usernames.length == 0){
             FXMLLoader loader = new FXMLLoader(Launcher.class.getResource(Utility.NO_RESULTS_FXML_PATH));
             try {
                 displayUsers.getChildren().add(loader.load());}
@@ -30,61 +29,48 @@ public class SearchFXML {
             ((NoResultsFXML)loader.getController()).initialize("No new posts from your followers yet...");
         }
         else{
-            for (Post post: sortedPost) {addPost(post);
-                Changer.removePostFromFeed(Loginner.loginnedUser.getUsername(), post.getPostID().getHandle());}
-        }
-
-        List<Comment> commentList = comments.stream().toList();
-        ArrayList<Comment> sortedComments = new ArrayList<>(commentList);
-        sortedComments.sort(Comparator.comparing(Comment::getDate));
-
-        if (sortedComments.size() == 0){
-            FXMLLoader loader = new FXMLLoader(Launcher.class.getResource(Utility.NO_RESULTS_FXML_PATH));
+            FXMLLoader loader = new FXMLLoader(Launcher.class.getResource(Utility.USERS_FXML_PATH));
             try {
-                displayDirects.getChildren().add(loader.load());}
+                displayUsers.getChildren().add(loader.load());}
             catch (IOException e) {e.printStackTrace();}
-            ((NoResultsFXML)loader.getController()).initialize("No new comments on your posts yet...");
-        }
-        else{
-            for (Comment comment: sortedComments) {addComment(comment);
-                Changer.removeCommentFromFeed(Loginner.loginnedUser.getUsername(), comment.getCommentID().getHandle());}
+            ((UsersFXML)loader.getController()).initialize(usernames);
         }
 
-        if (likes.size() == 0){
-            FXMLLoader loader = new FXMLLoader(Launcher.class.getResource(Utility.NO_RESULTS_FXML_PATH));
-            try {
-                displayGroups.getChildren().add(loader.load());}
-            catch (IOException e) {e.printStackTrace();}
-            ((NoResultsFXML)loader.getController()).initialize("No new likes on your posts yet...");
-        }
-        else{
-            for (Like like: likes) {addLike(like);
-                Changer.removeLikeFromFeed(Loginner.loginnedUser.getUsername(), like.getLikeID().getHandle());}
-        }
+        int[] directs = Loader.searchDirects(Loginner.loginnedUser.getUsername(), pattern);
+        Message[] messages = new Message[directs.length];
+        int i = 0;
+        for (int direct : directs) messages[i++] = MessageBuilder.getMessageFromDatabase(direct);
+
+        List<Message> directsList = Arrays.stream(messages).toList();
+        ArrayList<Message> sortedDirects = new ArrayList<>(directsList);
+        sortedDirects.sort(Comparator.comparing(Message::getDate));
+        for (Message message : sortedDirects) addMessageDirect(message);
+
+        int[] groups = Loader.searchGroups(Loginner.loginnedUser.getUsername(), pattern);
+        GroupMessage[] groupMessages = new GroupMessage[groups.length];
+        i = 0;
+        for (int group_message: groups) groupMessages[i++] = GroupMessageBuilder.getGroupMessageFromDatabase(group_message);
+
+        List<GroupMessage> groupMessageList = Arrays.stream(groupMessages).toList();
+        ArrayList<GroupMessage> sortedGroups = new ArrayList<>(groupMessageList);
+        sortedGroups.sort(Comparator.comparing(GroupMessage::getDate));
+        for (GroupMessage message : sortedGroups) addMessageGroup(message);
     }
 
-    private void addLike(Like like) {
-        FXMLLoader fxmlLoader = new FXMLLoader(Launcher.class.getResource(Utility.LIKE_FXML_PATH));
+    private void addMessageGroup(GroupMessage message) {
+        FXMLLoader fxmlLoader = new FXMLLoader(Launcher.class.getResource(Utility.MESSAGE_FXML_PATH));
         try {
-            displayUsers.getChildren().add(fxmlLoader.load());} catch (IOException e) {AppManager.alert(Alert.AlertType.ERROR,
+            displayDirects.getChildren().add(fxmlLoader.load());} catch (IOException e) {AppManager.alert(Alert.AlertType.ERROR,
                 "Exception occurred.", e.getCause().getMessage(), "Exception"); e.printStackTrace(); return;}
-        ((LikeFXML)fxmlLoader.getController()).initialize(like);
+        ((MessageFXML)fxmlLoader.getController()).initialize(message, UserBuilder.getUserFromDatabase(message.getUsername()));
     }
 
-    private void addPost(Post post) {
-        FXMLLoader fxmlLoader = new FXMLLoader(Launcher.class.getResource(Utility.POST_FXML_PATH));
+    private void addMessageDirect(Message message) {
+        FXMLLoader fxmlLoader = new FXMLLoader(Launcher.class.getResource(Utility.MESSAGE_FXML_PATH));
         try {
-            displayUsers.getChildren().add(fxmlLoader.load());} catch (IOException e) {AppManager.alert(Alert.AlertType.ERROR,
+            displayDirects.getChildren().add(fxmlLoader.load());} catch (IOException e) {AppManager.alert(Alert.AlertType.ERROR,
                 "Exception occurred.", e.getCause().getMessage(), "Exception"); e.printStackTrace(); return;}
-        ((PostFXML)fxmlLoader.getController()).initialize(post);
-    }
-
-    private void addComment(Comment comment) {
-        FXMLLoader fxmlLoader = new FXMLLoader(Launcher.class.getResource(Utility.POST_FXML_PATH));
-        try {
-            displayUsers.getChildren().add(fxmlLoader.load());} catch (IOException e) {AppManager.alert(Alert.AlertType.ERROR,
-                "Exception occurred.", e.getCause().getMessage(), "Exception"); e.printStackTrace(); return;}
-        ((PostFXML)fxmlLoader.getController()).initialize(comment);
+        ((MessageFXML)fxmlLoader.getController()).initialize(message, UserBuilder.getUserFromDatabase(message.getUsername()));
     }
 
 
