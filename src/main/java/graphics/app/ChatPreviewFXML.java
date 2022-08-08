7a4dc73c05667bbb30e518f64d40eb006c427294
AradMNK <aradkashani@yahoo.com>
2022-08-08 16:20:43 +0430

    final edits

--- src/main/java/graphics/app/ChatPreviewFXML.java
+++ src/main/java/graphics/app/ChatPreviewFXML.java
@@ -1,11 +1,14 @@
 package graphics.app;
 
+import Database.Changer;
+import Database.Loader;
 import Login.Loginner;
 import animatefx.animation.Pulse;
 import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.ImagePattern;
@@ -27,6 +30,7 @@ public class ChatPreviewFXML {
     @FXML Circle picture;
     @FXML Text name, header, content;
     @FXML GridPane picturePane, chatPane;
+    @FXML Label unreadLabel;
 
     private void initContents(String name, String header, String content){
         this.name.setText(name);
@@ -57,6 +61,10 @@ public class ChatPreviewFXML {
         if (dm.getRecipient().getUsername().equals(Loginner.loginnedUser.getUsername()))
             initContents(Utility.SAVED_MESSAGES, "", message.getContent());
         else initContents(user.getName(), "", message.getContent());
+
+        int unread = Loader.getUnreadCountForUsername(dm.getUser().getUsername(), dm.getRecipient().getUsername());
+        if (unread == 0) unreadLabel.setVisible(false);
+        else unreadLabel.setText(Integer.toString(unread));
     }
 
     public void initialize(Group group, GroupMessage message){
@@ -73,6 +81,9 @@ public class ChatPreviewFXML {
                         (Launcher.class.getResource(Utility.GROUP_PICTURE_PATH))).toString())));}
         }
         initContents(group.getName(), Database.Loader.getUserName(message.getUsername()), message.getContent());
+        int unread = Loader.getUnreadCountForGroupID(dm.getUser().getUsername(), group.getGroupID().getHandle());
+        if (unread == 0) unreadLabel.setVisible(false);
+        else unreadLabel.setText(Integer.toString(unread));
     }
     public void initialize(Group group) {
         isGroupType = true;
@@ -88,16 +99,23 @@ public class ChatPreviewFXML {
                         (Launcher.class.getResource(Utility.GROUP_PICTURE_PATH))).toString())));}
         }
         initContents(group.getName(), "", "(No one has messaged in this group yet...)");
+        int unread = Loader.getUnreadCountForGroupID(dm.getUser().getUsername(), group.getGroupID().getHandle());
+        if (unread == 0) unreadLabel.setVisible(false);
+        else unreadLabel.setText(Integer.toString(unread));
     }
 
     @FXML void hoverChatPane(){new Pulse(chatPane).play();}
-
     @FXML void clickChatPane(){
         FXMLLoader fxmlLoader = new FXMLLoader(Launcher.class.getResource(Utility.CHAT_FXML_PATH));
         try {MainFXML.root.setDisplayTo(fxmlLoader.load());} catch (IOException e) {AppManager.alert(Alert.AlertType.ERROR,
                 "Exception occurred.", e.getCause().getMessage(), "Exception"); e.printStackTrace(); return;}
-        if (isGroupType) ((ChatFXML)fxmlLoader.getController()).initialize(group);
-        else ((ChatFXML)fxmlLoader.getController()).initialize(dm);
+        if (isGroupType){
+            ((ChatFXML)fxmlLoader.getController()).initialize(group);
+            Changer.userSees(Loginner.loginnedUser.getUsername(), group.getGroupID().getHandle());
+        } else {
+            ((ChatFXML)fxmlLoader.getController()).initialize(dm);
+            Changer.userSees(Loginner.loginnedUser.getUsername(), dm.getRecipient().getUsername());
+        }
     }
 
     void changeChatPaneClickToForward(){chatPane.setOnMouseClicked(e->clickChatPaneToForward());}
